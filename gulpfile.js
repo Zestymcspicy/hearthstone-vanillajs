18414f611de94254c7cd80ffde45343d88f6728a
Harold Beckerman <halbeckerman@yahoo.com>
2019-06-26 22:50:48 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict'
+var gulp = require('gulp');
+var browsersync = require('browser-sync').create();
+
+gulp.task("serve", function() {
+  browsersync.init({
+    server: "./",
+  })
+})
+
+gulp.task('default', ['serve'], function() {
+  gulp.watch("*.html", browsersync.reload)
+  gulp.watch("css/styles.css", browsersync.reload)
+  gulp.watch("js/index.js", browsersync.reload)
+})
