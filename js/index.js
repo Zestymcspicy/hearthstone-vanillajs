2042cde2a1843075dabba2d6381a5593989ff50e
Harold Beckerman <halbeckerman@yahoo.com>
2019-06-30 00:35:22 -0500

    contact page is responsive. TODO?: clean up animation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 const hamburger = document.getElementById("hamburger");
 const navList = document.getElementById("nav-list");
+const openEmailFormButton = document.querySelector("#openEmailForm");
+const cancelEmailButton = document.querySelector("#cancelButton")
+const emailForm = document.querySelector("#emailForm");
+const mapContactContainer = document.querySelector("#mapContactContainer");
+
 
 hamburger.addEventListener('click', event => {
   navList.classList.toggle("show-menu")
 });
+
+cancelEmailButton.addEventListener('click', e => toggleContent(e))
+openEmailFormButton.addEventListener('click', e => toggleContent(e))
+
+const toggleContent = e => {
+  e.preventDefault()  
+  emailForm.classList.toggle('show');
+  emailForm.classList.toggle('hide');
+  mapContactContainer.classList.toggle('hide');
+  mapContactContainer.classList.toggle('show');
+};
