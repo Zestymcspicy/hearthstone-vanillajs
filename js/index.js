b91e8fe5be65e995a15c7790a1f7e1737324b4f2
Zestymcspicy <halbeckerman@yahoo.com>
2019-10-09 21:30:34 -0500

    announcement modal

--- js/index.js
+++ js/index.js
@@ -24,3 +24,32 @@ const toggleContent = e => {
   mapContactContainer.classList.toggle('hide');
   mapContactContainer.classList.toggle('show');
 };
+// if(window.localStorage)
+if(document.cookie.indexOf("seenAlumniBreakfast2019=true")===-1) {
+  document.cookie = "seenAlumniBreakfast2019=true; max-age=3600";
+  addAnnouncementModal();
+}
+
+function addAnnouncementModal(){
+  let announcementModal = `<div class="modal-background">
+  <div class="modal">
+  <h2>Hey Alumni!</h2>
+  <p>The Alumni Breakfast is scheduled for November 2nd at 10am. Hope to see you there!</p>
+  <button id="modalCloseButton" class="modal-button">OK</button>
+  </div>
+  </div>`
+  document.querySelector("#backgroundImage").insertAdjacentHTML('afterend', announcementModal)
+  document.querySelector(".modal-background").addEventListener("click", e => closeModal(e))
+  document.querySelector("#modalCloseButton").addEventListener("click", e => closeModal(e))
+  let modal = document.querySelector(".modal")
+  modal.addEventListener("click", e => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    return false;
+  })
+}
+
+function closeModal(e) {
+  document.querySelector(".modal-background").style.display="none";
+}
